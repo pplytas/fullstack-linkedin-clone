376892008186c00439dd75f8e4a471e0ff0b5025
dgounaris <dimitris.gounaris@hotmail.com>
2018-05-26 20:06:26 +0200

    Fixed some minor warnings

--- tedi_server/src/main/java/server/TediServerApplication.java
+++ tedi_server/src/main/java/server/TediServerApplication.java
@@ -1,11 +1,7 @@
 package server;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import server.auth.UserService;
-import server.entities.UserEntity;
 
 @SpringBootApplication
 public class TediServerApplication {
