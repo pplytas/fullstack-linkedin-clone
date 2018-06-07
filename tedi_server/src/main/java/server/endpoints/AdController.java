22c116c1731a1ba4234f1804cc8777e4d8942392
dgounaris <dimitris.gounaris@hotmail.com>
2018-06-07 13:32:39 +0200

    Fixed some warnings

--- tedi_server/src/main/java/server/endpoints/AdController.java
+++ tedi_server/src/main/java/server/endpoints/AdController.java
@@ -1,5 +1,7 @@
 package server.endpoints;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
