0f211ae70f3e1c4576dca3d5bcf78de1df0b1678
gr_wellcome <panagiotis.plytas@gmail.com>
2018-07-26 01:33:57 +0300

    Update http request method in front-end

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,9 +1,28 @@
 (function() {
 
-	var app = angular.module('tediApp', [])
+	var app = angular.module('tediApp', [
+		// 'angular',
+		// 'angular-route',
+		'angular-jwt'])
+	.config(function($httpProvider, jwtOptionsProvider) {
+		// /* ================= JWT ================= */
+		// jwtOptionsProvider.config({
+		// 	tokenGetter: function(options) {
+		// 		token = localStorage.getItem('isjwt');
+		// 		if (!token) {
+		// 			window.location.href = "/login";
+		// 		}
+		// 		return token;
+		// 	},
+		// 	whiteListedDomains: ['apiv1.ismood.com', 'localhost']
+		// });
+		// $httpProvider.interceptors.push('jwtInterceptor');
+		// /* ======================================= */
+
+	})
 	.run(function ($rootScope, globalFunctions) {
 		globalFunctions.init_app();
-        globalFunctions.login('p@root.com', 'toor');
+        globalFunctions.login("d@root.com", "toor");
 	});
 
 })();
--- front-end/js/services/generalService.js
+++ front-end/js/services/generalService.js
@@ -13,31 +13,67 @@
 			var url = $rootScope.tediAPI + endpoint;
 
 			var parameter = {
-				email: inputEmail,
-                password: inputPassword
+				"email": inputEmail,
+				"password": inputPassword
 			};
+
+			// $.ajax({
+			// 	type: "POST",
+			// 	url: url,
+			// 	contentType: "application/json; charset=utf-8",
+			// 	data: parameter,
+			// 	success : function(data) {
+			// 		localStorage.isjwt = data.token;
+			// 		console.log("Login Successful");
+			// 	}
+			// });
 
 			return $http({
 				method: "POST",
 				url: url,
                 headers: { 'Content-Type': 'application/json' },
-                params: parameter
+                data: parameter,
 			}).then(function onSuccess(response) {
-                // Handle success
-                console.log(response);
-                console.log("Login Successful");
-                $http({
-    				method: "GET",
-    				url: $rootScope.tediAPI + "/admin/userlist"
-    			}).then(function onSuccess(response) {
-                    console.log(response);
-                })
-              }).catch(function onError(response) {
-                // Handle error
-                console.log(response);
-                console.log("Login Failed");
-              });
+				// Handle success
+				console.log(response);
+				console.log(response.headers());
+				console.log(response.headers("Authorization"));
+				console.log("Login Successful");
+			}).catch(function onError(response) {
+				// Handle error
+				console.log(response);
+				console.log(response.headers());
+				console.log("Login Failed");
+			});
 
+
+			// $.ajax({
+  			//    url: "https://localhost:8443/login?email="+inputEmail+"&password="+"toor",
+  			//    data: {
+  			//       format: 'json'
+  			//    },
+  			//    error: function(response) {
+  			//       console.log(response);
+  			//    },
+  			//    dataType: 'json',
+  			//    success: function(data) {
+  			//       console.log(data);
+  			//    },
+  			//    type: 'POST'
+  			// });
+  			// var xhttp = new XMLHttpRequest();
+  			// xhttp.open("POST", "https://localhost:8443/login", true);
+  			// xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  			// xhttp.send("email="+inputEmail+"&password="+inputPassword);
+  			//
+  			// xhttp.onreadystatechange = function() {
+  			// 	if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+  			// 		console.log("1");
+  			// 		var headers = this.getAllResponseHeaders();
+  			// 		console.log(headers);
+  			// 		console.log(this);
+  			// 	}
+  			// };
 		}
 
 		return globalFunctionsFactory;
