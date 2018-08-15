e5bb8d52f9d58a090494eb4929c11d826c8a9aa5
dgounaris <dimitris.gounaris@hotmail.com>
2018-08-15 20:11:46 +0300

    Added extra fields in articleOutput, waiting for frontend merge to combine fixes

--- tedi_server/src/main/java/server/endpoints/outputmodels/UpvoteOutputModel.java
+++ tedi_server/src/main/java/server/endpoints/outputmodels/UpvoteOutputModel.java
@@ -2,16 +2,34 @@ package server.endpoints.outputmodels;
 
 public class UpvoteOutputModel {
 	
-	private String upvoter; //currently email, may change to name later on
+	private String upvoterName;
+	private String upvoterSurname;
+	private String upvoterEmail; //currently email, may change to name later on
 	
 	public UpvoteOutputModel() {}
 
-	public String getUpvoter() {
-		return upvoter;
+	public String getUpvoterName() {
+		return upvoterName;
 	}
 
-	public void setUpvoter(String upvoter) {
-		this.upvoter = upvoter;
+	public String getUpvoterSurname() {
+		return upvoterSurname;
+	}
+
+	public String getUpvoterEmail() {
+		return upvoterEmail;
+	}
+
+	public void setUpvoterName(String upvoterName) {
+		this.upvoterName = upvoterName;
+	}
+
+	public void setUpvoterSurname(String upvotedSurname) {
+		this.upvoterSurname = upvotedSurname;
+	}
+
+	public void setUpvoterEmail(String upvoterEmail) {
+		this.upvoterEmail = upvoterEmail;
 	}
 
 }
