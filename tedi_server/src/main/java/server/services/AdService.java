cba179351c78f85be4fe9fbc39626c72d4f4a8e1
dgounaris <dimitris.gounaris@hotmail.com>
2018-09-24 22:06:14 +0300

    Add delete and massive query endpoints for applications

--- tedi_server/src/main/java/server/services/AdService.java
+++ tedi_server/src/main/java/server/services/AdService.java
@@ -1,13 +1,19 @@
 package server.services;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import server.endpoints.outputmodels.AdApplicationOutputModel;
 import server.endpoints.outputmodels.AdOutputModel;
 import server.endpoints.outputmodels.SkillOutputModel;
 import server.endpoints.outputmodels.UserOutputModel;
+import server.entities.AdApplicationEntity;
 import server.entities.AdEntity;
 import server.entities.AdSkillEntity;
 import server.utilities.StorageManager;
@@ -38,6 +44,24 @@ public class AdService {
 			adOut.addSkill(sOut);
 		}
 		return adOut;
+	}
+
+	public List<AdApplicationOutputModel> adAppToOutputModel(List<AdApplicationEntity> adApplications) {
+		List<AdApplicationOutputModel> output = new ArrayList<>();
+		for (AdApplicationEntity adApp : adApplications) {
+			AdApplicationOutputModel adAppOut = new AdApplicationOutputModel();
+			adAppOut.setId(adApp.getId());
+			AdOutputModel adOut = adToOutputModel(adApp.getAd());
+			adAppOut.setAd(adOut);
+			try {
+				adAppOut.setUser(userEntityService.getUserOutputModelFromUser(adApp.getUser()));
+			} catch (IOException e) {
+				adAppOut.setUser(userEntityService.getSafeUserOutputModelFromUser(adApp.getUser()));
+			}
+			adAppOut.setStatus(adApp.getStatus());
+			output.add(adAppOut);
+		}
+		return output;
 	}
 
 }
